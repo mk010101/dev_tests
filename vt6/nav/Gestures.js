6a882af3b0dfe63bc05de92bfc9ed606c10ac067
Max Kostin <mkostin@videotel.com>
2019-10-09 16:00:47 +0100

    p

--- vt6/nav/Gestures.js
+++ vt6/nav/Gestures.js
@@ -15,10 +15,12 @@ class Gestures extends Dispatcher {
         this.target = target;
         this._x0 = 0;
         this._y0 = 0;
+        this._speedX0 = 0;
         this._speedX = 0;
+        this._speedY0 = 0;
         this._speedY = 0;
         this._detectPanDist = 10;
-        this._isSwiping = false;
+        this._isPanningX = false;
         this._gestures = [];
         this._targetBB = null;
 
@@ -48,14 +50,15 @@ class Gestures extends Dispatcher {
 
         if ('ontouchstart' in window) {
 
-            //this.touchStart = this.touchStart.bind(this);
-            //this.touchEnd = this.touchEnd.bind(this);
-
             this.target.addEventListener("touchstart", this.pointerDown);
             window.addEventListener("touchend", this.pointerUp);
 
         }
 
+    }
+
+    get speedX() {
+        return this._speedX;
     }
 
 
@@ -73,6 +76,9 @@ class Gestures extends Dispatcher {
 
         }
 
+        this._speedX0 = clientX;
+        this._speedY0 = clientY;
+
         this._x0 = clientX - this._targetBB.left;
         this._y0 = clientY - this._targetBB.top;
 
@@ -81,6 +87,22 @@ class Gestures extends Dispatcher {
     }
 
     pointerMove(e) {
+
+        let clientX, clientY;
+
+        if (e.type.indexOf("touch") > -1) {
+            clientX = e.touches[0].clientX;
+            clientY = e.touches[0].clientY;
+        } else {
+            clientX = e.clientX;
+            clientY = e.clientY;
+        }
+
+        this._speedX = this._speedX0 - clientX;
+        this._speedY = this._speedY0 - clientY;
+        this._speedX0 = clientX;
+        this._speedY0 = clientY;
+
         for (let i = 0; i < this._gestures.length; i++) {
             this[this._gestures[i]](e);
         }
@@ -88,11 +110,15 @@ class Gestures extends Dispatcher {
 
 
     pointerUp(e) {
+        this.removeMoveListeners();
         document.querySelector(".page").classList.remove("no-scroll");
+        console.log(e)
+    }
+
+    removeMoveListeners() {
         this.target.removeEventListener("mousemove", this.pointerMove);
         this.target.removeEventListener("touchmove", this.pointerMove);
-        this._isSwiping = false;
-
+        this._isPanningX = false;
     }
 
 
@@ -117,19 +143,17 @@ class Gestures extends Dispatcher {
         let y = clientY - this._y0 - this._targetBB.top;
 
 
-        if (!this._isSwiping) {
+        if (!this._isPanningX) {
 
             let xa = Math.abs(x);
             let ya = Math.abs(y);
-            //console.log(xa, ya)
 
             if (ya > this._detectPanDist && ya > xa) {
-                this.pointerUp(e);
+                this.removeMoveListeners();
             } else if (xa > this._detectPanDist && xa > ya) {
-                this._isSwiping = true;
+                this._isPanningX = true;
                 document.querySelector(".page").classList.add("no-scroll");
                 this.dispatch("panXStart", e);
-                //console.log(x, clientX, this._x0)
             }
         } else {
             this.target.style.transform = `translateX(${x}px)`;
